cde9a1944cdddeaa5f596129b3cbcbd6d926f773
Hugo Bel <hugobel.nqn@gmail.com>
2024-01-03 16:08:00 -0300

    Menu según Rol de acceso

--- view/MainNav/nav.php
+++ view/MainNav/nav.php
@@ -1,25 +1,48 @@
-
-<nav class="side-menu">
-    <ul class="side-menu-list">
-        <li class="blue-dirty">
-            <a href="..\home\">
-                <span class="glyphicon glyphicon-th"></span>
-                <span class="lbl">Inicio</span>
-            </a>
-        </li>
-        <li class="blue-dirty">
-            <a href="..\nuevoTicket\">
-                <span class="glyphicon glyphicon-th"></span>
-                <span class="lbl">Nuevo Ticket</span>
-            </a>
-        </li>
-
-        <li class="blue-dirty">
-            <a href="..\consultarTicket\">
-                <span class="glyphicon glyphicon-th"></span>
-                <span class="lbl">Consultar Ticket</span>
-            </a>
-        </li>
-    </ul>
-</nav>
 <?php
+    if ($_SESSION["rol_id"]==1){
+        ?>
+            <nav class="side-menu">
+                <ul class="side-menu-list">
+                    <li class="blue-dirty">
+                        <a href="..\home\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Inicio</span>
+                        </a>
+                    </li>
+                    <li class="blue-dirty">
+                        <a href="..\nuevoTicket\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Nuevo Ticket</span>
+                        </a>
+                    </li>
+
+                    <li class="blue-dirty">
+                        <a href="..\consultarTicket\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Consultar Ticket</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        <?php
+    }else{
+        ?>
+            <nav class="side-menu">
+                <ul class="side-menu-list">
+                    <li class="blue-dirty">
+                        <a href="..\home\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Inicio</span>
+                        </a>
+                    </li>
+                    <li class="blue-dirty">
+                        <a href="..\consultarTicket\">
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span class="lbl">Consultar Ticket</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        <?php
+    }
+?>Z
