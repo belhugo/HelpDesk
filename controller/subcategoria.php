59f5115b1d65b9f1c2766b207506815a507bd0f4
Hugo Bel <hugobel.nqn@gmail.com>
2024-03-22 16:14:14 -0300

    Mantenimiento Prioridad, Categoría, Subcategoría. (CRUD)

--- controller/subcategoria.php
+++ controller/subcategoria.php
@@ -4,9 +4,56 @@
     $subcategoria = new Subcategoria();
 
     switch($_GET["op"]){
+
+        case "guardaryeditar":
+            if(empty($_POST["subcat_id"])){
+                $subcategoria->insert_subcategoria($_POST["cat_id"],$_POST["subcat_nom"]);     
+            }else {
+                $subcategoria->update_subcategoria($_POST["subcat_id"],$_POST["cat_id"],$_POST["subcat_nom"]);
+            }
+            break;
+
+        case "listar":
+            $datos=$subcategoria->get_subcategoria_all();
+            $data= Array();
+            foreach($datos as $row){
+                $sub_array = array();
+                $sub_array[] = $row["cat_nom"];
+                $sub_array[] = $row["subcat_nom"];
+                $sub_array[] = '<button type="button" onClick="editar('.$row["subcat_id"].');"  id="'.$row["subcat_id"].'" class="btn btn-inline btn-warning btn-sm ladda-button"><i class="fa fa-edit"></i></button>';
+                $sub_array[] = '<button type="button" onClick="eliminar('.$row["subcat_id"].');"  id="'.$row["subcat_id"].'" class="btn btn-inline btn-danger btn-sm ladda-button"><i class="fa fa-trash"></i></button>';
+                $data[] = $sub_array;
+            }
+
+            $results = array(
+                "sEcho"=>1,
+                "iTotalRecords"=>count($data),
+                "iTotalDisplayRecords"=>count($data),
+                "aaData"=>$data);
+            echo json_encode($results);
+            break;
+
+        case "eliminar":
+            $subcategoria->delete_subcategoria($_POST["subcat_id"]);
+            break;
+
+        case "mostrar";
+            $datos=$subcategoria->get_subcategoria_x_id($_POST["subcat_id"]);  
+            if(is_array($datos)==true and count($datos)>0){
+                foreach($datos as $row)
+                {
+                    $output["subcat_id"] = $row["subcat_id"];
+                    $output["cat_id"] = $row["cat_id"];
+                    $output["subcat_nom"] = $row["subcat_nom"];
+                }
+                echo json_encode($output);
+            }
+            break;
+
         case "combo":
             $datos = $subcategoria->get_subcategoria($_POST["cat_id"]);
-            $html="<option label='Seleccionar'></option>";
+            $html="";
+            $html.="<option label='Seleccionar'></option>";
             if(is_array($datos)==true and count($datos)>0){
                 foreach($datos as $row)
                 {
@@ -14,6 +61,6 @@
                 }
                 echo $html;
             }
-        break;
+            break;
     }
 ?>
--- models/Subcategoria.php
+++ models/Subcategoria.php
@@ -11,5 +11,75 @@
             return $resultado=$sql->fetchAll();
         }
 
+        public function get_subcategoria_all(){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT 
+            tm_subcategoria.subcat_id,
+            tm_subcategoria.cat_id,
+            tm_subcategoria.subcat_nom,
+            tm_categoria.cat_nom
+            FROM tm_subcategoria INNER JOIN
+            tm_categoria on tm_subcategoria.cat_id = tm_categoria.cat_id
+            WHERE tm_subcategoria.est=1";
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        /* TODO:Insert */
+        public function insert_subcategoria($cat_id,$subcat_nom){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO tm_subcategoria (subcat_id,cat_id,subcat_nom,est) VALUES (NULL,?,?,'1');";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $cat_id);
+            $sql->bindValue(2, $subcat_nom);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        /* TODO:Update */
+        public function update_subcategoria($subcat_id,$cat_id,$subcat_nom){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="UPDATE tm_subcategoria set
+                cat_id = ?,
+                subcat_nom = ?
+                WHERE
+                subcat_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $cat_id);
+            $sql->bindValue(2, $subcat_nom);
+            $sql->bindValue(3, $subcat_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        /* TODO:Delete */
+        public function delete_subcategoria($subcat_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="UPDATE tm_subcategoria SET
+                est = 0
+                WHERE 
+                subcat_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $subcat_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        /* TODO:Registro x id */
+        public function get_subcategoria_x_id($subcat_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM tm_subcategoria WHERE subcat_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $subcat_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
     }
 ?>
